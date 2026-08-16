bd0b5fcf6dc4e9364261700288b052ce19278670
rteitch <rizaltaufiq85@gmail.com>
2026-08-16 14:06:17 +0700

    feat: add brand favicon and update PWA manifest

--- sw.js
+++ sw.js
@@ -4,6 +4,7 @@ const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
   '/manifest.json',
+  '/favicon.svg',
   'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap'
 ];
 
